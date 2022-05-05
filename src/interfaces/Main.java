b13597cbb85dcb11ff37a523e34999360a1afc64
Tine-m <tinemarbjerg@gmail.com>
2022-05-05 15:19:06 +0200

    WAREHOUSEiMPL finish

--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -3,6 +3,9 @@ package interfaces;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Warehouse warehouse = new WarehouseImpl();
+        Order order = new Order("TALISKER", 51);
+        boolean result = order.fill(new WarehouseImpl());
+        System.out.println(result);
     }
 }
--- src/interfaces/WarehouseImpl.java
+++ src/interfaces/WarehouseImpl.java
@@ -8,33 +8,40 @@ public class WarehouseImpl implements Warehouse {
 
   public WarehouseImpl() {
     this.inventory = new ArrayList<>();
+    populateWarehouse();
   }
 
-  public void populateWarehouse() {
+  private void populateWarehouse() {
     inventory.add(new Product("TALISKER", 50));
   }
 
   @Override
   public boolean hasInventory(String productName, int quantity) {
-    for (Product p : inventory
-    ) {
-      if (p.getProductName().equals(productName)) {
+    Product tmp = findProduct(productName);
+    if (tmp != null) {
+      if (tmp.getQuantityOnHand() >= quantity) {
+        return true;
+      } else return false; // not enough items in stock
+    } else return false; // no such product
+  }
+
+  @Override
+  public boolean remove(String productName, int quantity) {
+    for (Product product : inventory) {
+      if (product.getProductName().equals(productName)) {
+        product.remove(quantity);
         return true;
       }
     }
     return false;
   }
 
-  @Override
-  public boolean remove(String productName, int quantity) {
-    Product tmp = new Product(productName, quantity);
-    for (Product product : inventory
-    ) {
+  private Product findProduct(String productName) {
+    for (Product product : inventory) {
       if (product.getProductName().equals(productName)) {
-        product.remove(quantity);
-      return true;
+        return product;
       }
     }
-    return false;
+    return null;
   }
 }
